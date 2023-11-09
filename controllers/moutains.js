ff7936120c703bf80cff90cf480d6c963f334ec2
Sai Kumar maram <137809859+Saikumr23@users.noreply.github.com>
2023-11-08 23:01:34 -0600

    modified

--- controllers/moutains.js
+++ controllers/moutains.js
@@ -27,3 +27,37 @@ res.send('NOT IMPLEMENTED: moutains delete DELETE ' + req.params.id);
 exports.moutains_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: moutains update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.moutains_view_all_Page = async function(req, res) {
+    try{
+    themoutains = await moutains.find();
+    res.render('moutains', { title: 'moutains Search Results', results: themoutains });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
+    // Handle moutains create on POST.
+exports.moutains_create_post = async function(req, res) {
+console.log(req.body)
+let document = new moutains();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"moutains_type":"goat", "cost":12, "size":"large"}
+document.mountainName = req.body.mountainName;
+document.Height = req.body.Height;
+document.place = req.body.place;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
--- routes/moutains.js
+++ routes/moutains.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const moutains_controlers= require('../controllers/moutains');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('moutains', { title: 'Search Results Moutains' });
-});
+});*/
+router.get('/', moutains_controlers.moutains_view_all_Page );
 
 module.exports = router;
+
+
+
